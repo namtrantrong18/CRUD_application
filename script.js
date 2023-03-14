d5c166b2559326eb8d6f011855b9f98873bdb25e
Nam Tran <58880637+namtrantrong18@users.noreply.github.com>
2023-03-15 02:07:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// var selectedRow = null;
-
-// // Show Alert
-// function showAlert(message, className) {
-//     const div = document.createElement('div');
-//     div.className = 'alert alert-$(className)';
-
-//     div.appendChild(document.createTextNode(message));
-//     const container = document.querySelector(".container");
-//     const main = document.querySelector(".main");
-//     container.insertBefore(div, main);
-
-//     setTimeout(() => document.querySelector(".alert").remove(), 3000);
-// }
-
 var studentApi = " http://localhost:3000/student"
 
 function start() {
